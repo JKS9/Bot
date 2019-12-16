6ae00e19c5086988fcbd87ea882085c2a85b6c21
JKS9 <juzans.etienne@gmail.com>
2019-12-16 14:49:03 +0100

    dark mode bote

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './styles/sass/index.scss';
+
 import * as MyConstants from '../static/constants';
+
 import Chat from './script/Chat';
 import Bot from '../static/json-mock/bot/bot_command_fr.json';
 
 const chat = new Chat(MyConstants.user, Bot);
-
-// convertir le message en minuscule
 chat.isBotCommandExist('DATddE');
